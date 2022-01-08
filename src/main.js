90f4f829f8c86c8a2f7846d2e2a9ffb1282faa62
joao-simoes <joaopsimoes0@gmail.com>
2022-01-08 12:41:55 +0000

    .

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VuePageTransition from "vue-page-transition";
+import VueImg from 'v-img';
 
+Vue.use(VueImg);
 Vue.use(VuePageTransition);
 
 Vue.config.productionTip = false
--- src/views/albums/Architecture.vue
+++ src/views/albums/Architecture.vue
@@ -3,26 +3,24 @@
     <div class="container-fluid pb-5">
       <div class="row d-flex justify-content-center">
         <div class="col-10 col-xxl-8 offset-0 offset-xl-0 offset-xxl-3">
-
-
           <div class="text-end text-lg-start mt-5">
             <router-link to="/photography/digital">
-            <i class="fas fa-backward fa-2x mb-4" style="color: #202020"></i
-          ></router-link>
+              <i class="fas fa-backward fa-2x mb-4" style="color: #202020"></i
+            ></router-link>
 
-          <h2>Arquitetura</h2>
+            <h2>Arquitetura</h2>
           </div>
 
           <div class="text-center">
             <div class="row my-5 py-2">
               <div class="col-12 col-lg-6">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/2.jpg"
                   alt="Arquitetura"
                 />
               </div>
               <div class="col-12 col-lg-6">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/3.jpg"
                   alt="Arquitetura"
                 />
@@ -31,7 +29,7 @@
 
             <div class="row my-5 py-2">
               <div class="col-12 text-center">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/1.jpg"
                   alt="Arquitetura"
                 />
@@ -40,13 +38,13 @@
 
             <div class="row my-5 py-2 align-items-center">
               <div class="col-12 col-lg-5">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/4.jpg"
                   alt="Arquitetura"
                 />
               </div>
               <div class="col-12 col-lg-7">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/5.jpg"
                   alt="Arquitetura"
                 />
@@ -55,47 +53,44 @@
 
             <div class="row my-5 py-2 align-items-center">
               <div class="col-12 col-lg-7">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/6.jpg"
                   alt="Arquitetura"
                 />
               </div>
               <div class="col-12 col-lg-5">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/11.jpg"
                   alt="Arquitetura"
                 />
               </div>
             </div>
 
-
             <div class="row my-5 py-2 align-items-center">
               <div class="col-12 col-lg-4">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/8.jpg"
                   alt="Arquitetura"
                 />
               </div>
               <div class="col-12 col-lg-4">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/9.jpg"
                   alt="Arquitetura"
                 />
               </div>
               <div class="col-12 col-lg-4">
-                <img
+                <img v-img:group
                   src="../../assets/img/photography/architecture/10.jpg"
                   alt="Arquitetura"
                 />
               </div>
             </div>
-
-            
           </div>
         </div>
       </div>
     </div>
-    <Footer/>
+    <Footer />
   </div>
 </template>
 
@@ -106,7 +101,7 @@ export default {
   name: "Architecture",
   components: {
     Footer,
-  },
+  }
 };
 </script>
 
